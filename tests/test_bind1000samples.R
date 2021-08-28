a9ff6ba4e79e3d04e4192bbbd33d6a3c9c7dd41b
Zoe Kao <syzoekao@gmail.com>
2021-08-27 22:29:57 -0400

    added more code for results

--- tests/test_bind1000samples.R
+++ tests/test_bind1000samples.R
@@ -27,15 +27,21 @@ system.time(county_ypll_age <- calculate_ypll(dt = death_samp, byvar = c("fips",
 
 
 ### For plot
+death_samp <- bind1000samples()
+year_rle <- NA
 
 usa_ypll_ls <- calculate_usa_ypll(dt = death_samp, year_rle = year_rle)
 
-var_vec <- c("state", "urban_rural_code", "svi_cate")
-axis_text_size <- c(8, 10, 12)
-axis_text_angle <- c(0, 0, 0)
-usa_text_angle <- c(90, 0, 0)
-width_vec <- c(14, 10, 10)
-height_vec <- c(14, 12, 12)
+death_samp[, urban_rural_svi := ifelse(!is.na(svi_cate),
+                                       paste0(urban_rural_code, ";\n", svi_cate, " SVI"), NA)]
+death_samp[, svi_cate := ifelse(!is.na(svi_cate), paste0(svi_cate, " SVI"), NA)]
+
+var_vec <- c("state", "urban_rural_code", "svi_cate", "urban_rural_svi")
+axis_text_size <- c(8, 10, 12, 10)
+axis_text_angle <- c(0, 0, 0, 90)
+usa_text_angle <- c(90, 0, 0, 0)
+width_vec <- c(14, 12, 12, 14)
+height_vec <- c(14, 12, 12, 14)
 
 for (i in c(1:length(var_vec))) {
 
@@ -44,8 +50,9 @@ for (i in c(1:length(var_vec))) {
                                            usa_ypll_ls = usa_ypll_ls, panel_letter = "(A)",
                                            usa_text_angle = usa_text_angle[i],
                                            axis.text.x.size = axis_text_size[i],
-                                           axis.text.x.angle = 0)
-  g_prop_by_age <- plot_prop_ypll_by_age(dt = death_samp, byvar = byvar, year_rle = NA,
+                                           axis.text.x.angle = axis_text_angle[i])
+  g_prop_by_age <- plot_prop_ypll_by_age(dt = death_samp, byvar = byvar,
+                                         n_age_breaks = 4, year_rle = NA,
                                          usa_ypll_ls = usa_ypll_ls,
                                          panel_letter = "(B)",
                                          axis.text.y.size = axis_text_size[i])
@@ -63,11 +70,33 @@ for (i in c(1:length(var_vec))) {
 
   wb <- createWorkbook()
   addWorksheet(wb, "Age-Adjusted YPLL Rate")
-  writeData(wb, "Age-Adjusted YPLL Rate", g_aa_ypll_rate$plot_data, startRow = 3, startCol = 1)
+  plot_data <- g_aa_ypll_rate$plot_data
+  keep_cols <- grep("_mean", colnames(plot_data), value = T)
+  keep_cols <- c(byvar, keep_cols)
+  plot_data <- plot_data[, ..keep_cols]
+  old_names <- c("covid_19_deaths_mean", "covid19_death_rate_mean", "covid19_death_rate_aa_mean",
+                 "tot_ypll_mean", "ypll_rate_mean", "ypll_rate_aa_mean")
+  setcolorder(plot_data, c(byvar, old_names))
+  setnames(plot_data, old_names, c("COVID-19 Deaths", "COVID-19 Death Rate", "Age-Adjusted COVID-19 Death Rate",
+                                   "Total YPLL", "YPLL Rate", "Age-Adjusted YPLL Rate"))
+  writeData(wb, "Age-Adjusted YPLL Rate", plot_data, startRow = 3, startCol = 1)
+
   addWorksheet(wb, "Age Proportion")
-  writeData(wb, "Age Proportion", g_prop_by_age$plot_data, startRow = 3, startCol = 1)
+  plot_data <- g_prop_by_age$plot_data
+  keep_cols <- c(byvar, "age_group_plot", "ypll", "total_ypll", "prop_ypll_plot")
+  plot_data <- plot_data[, ..keep_cols]
+  setnames(plot_data, c("age_group_plot", "ypll", "total_ypll", "prop_ypll_plot"),
+           c("Age Group", "Total YPLL", "Total YPLL by Category", "Proportion of Total YPLL Attributable to Age Group"))
+  writeData(wb, "Age Proportion", plot_data, startRow = 3, startCol = 1)
+
   addWorksheet(wb, "Ratio YPLL to Death")
-  writeData(wb, "Ratio YPLL to Death", g_ratio$plot_data, startRow = 3, startCol = 1)
+  plot_data <- g_ratio$plot_data
+  keep_cols <- c(byvar, "covid_19_deaths", "prop_covid", "tot_ypll", "prop_ypll", "ratio")
+  plot_data <- plot_data[, ..keep_cols]
+  setnames(plot_data, c("covid_19_deaths", "prop_covid", "tot_ypll", "prop_ypll", "ratio"),
+           c("COVID-19 Deaths", "% COVID-19 Deaths Attributable to Category",
+             "Total YPLL", "% Total YPLL Attributable to Category", "Ratio of YPLL to COVID-19 Deaths"))
+  writeData(wb, "Ratio YPLL to Death", plot_data, startRow = 3, startCol = 1)
   saveWorkbook(wb, file = paste0("inst/plot_data (", byvar, ").xlsx"), overwrite = T)
 }
 
